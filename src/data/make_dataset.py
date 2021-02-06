b0d947ebecf6d2d9f4f92ef6b0d8cc99de25ee6f
Ben Torvaney <torvaney@protonmail.com>
2021-02-06 23:37:33 +0000

    Add noisy voronoi to registry

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -60,6 +60,7 @@ def main(input_filepath, output_filepath):
             'basic': basic.create_image,
             'triangle': basic.create_image_shot_angle_only,
             'voronoi': voronoi.create_image_voronoi,
+            'noisy_voronoi': voronoi.create_image_voronoi_noisy,
             'cropped_voronoi': voronoi.create_image_voronoi_cropped,
             'minimal_voronoi': voronoi.create_image_minimal_voronoi,
         }
