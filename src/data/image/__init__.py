ccffaa06c7283e5e1088dca6df3d08334b03fd5b
Ben Torvaney <torvaney@protonmail.com>
2021-03-01 14:44:12 +0000

    Run model subset

--- src/data/image/__init__.py
+++ src/data/image/__init__.py
@@ -4,9 +4,9 @@ from src.data.image import basic, common, voronoi
 IMAGE_TYPES = {
     'basic': basic.create_image,
     'triangle': basic.create_image_shot_angle_only,
-    'bubbles': basic.create_image_opponent_bubbles,
     'voronoi': voronoi.create_image_voronoi,
     'noisy_voronoi': voronoi.create_image_voronoi_noisy,
-    'cropped_voronoi': voronoi.create_image_voronoi_cropped,
     'minimal_voronoi': voronoi.create_image_minimal_voronoi,
+    # 'bubbles': basic.create_image_opponent_bubbles,
+    # 'cropped_voronoi': voronoi.create_image_voronoi_cropped,
 }
